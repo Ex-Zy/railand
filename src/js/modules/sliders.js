6a6def4f9c553b7ede5167b014410f193adac4cc
neversaynever44 <killy-08@mail.ru>
2018-10-15 02:37:49 +0300

    merge

--- src/js/modules/sliders.js
+++ src/js/modules/sliders.js
@@ -2,14 +2,8 @@ const reviewsSlider = $('.js-reviews-slider');
 
 
 function initSlider(slider, options) {
-  slider.on('init', function() {
-    setTimeout(function() {
-      slider.addClass('is-ready');
-    }, 100);
-  });
   slider.not('.slick-initialized').slick(options);
 }
-
 
 initSlider(reviewsSlider, {
   slidesToShow: 1,
