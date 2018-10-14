83b8bf202ead7395920d46506383c8ffb3f149fd
neversaynever44 <killy-08@mail.ru>
2018-10-15 02:32:14 +0300

    about-page

--- src/js/modules/sliders.js
+++ src/js/modules/sliders.js
@@ -2,8 +2,14 @@ const reviewsSlider = $('.js-reviews-slider');
 
 
 function initSlider(slider, options) {
+  slider.on('init', function() {
+    setTimeout(function() {
+      slider.addClass('is-ready');
+    }, 100);
+  });
   slider.not('.slick-initialized').slick(options);
 }
+
 
 initSlider(reviewsSlider, {
   slidesToShow: 1,
