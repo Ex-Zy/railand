47241140b54133b548e7f08525c6318ece5d65d0
neversaynever44 <killy-08@mail.ru>
2018-10-15 15:34:29 +0300

    add cards with sliders on catalog-page

--- src/js/modules/sliders.js
+++ src/js/modules/sliders.js
@@ -1,6 +1,5 @@
 const reviewsSlider = $('.js-reviews-slider');
 
-
 function initSlider(slider, options) {
   slider.not('.slick-initialized').slick(options);
 }
@@ -11,4 +10,17 @@ initSlider(reviewsSlider, {
   speed: 800,
   autoplaySpeed: 4000,
   dots: true
+});
+
+// card sliders
+let numSlick = 0;
+$('.card__slider').each( function() {
+  numSlick++;
+  $(this).addClass( 'js-card-slider-' + numSlick ).slick({
+    slidesToShow: 1,
+    speed: 800,
+    dots: true,
+    autoPlay: true,
+    autoplaySpeed: 4000
+  });
 });
